f47c81aad43898c2af40ba54ed97dec5a164e1a1
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-07-01 21:10:51 +0400

    did a lot of stuff from 'optimizations' branch because i deleted that due to too many merge conflicts

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,8 +1,11 @@
 extends TextureButton
 
 export var scene_path: String
+export var exit_button: bool
 
 
 func _on_button_up() -> void:
-	get_tree().change_scene(scene_path)
-
+	if exit_button:
+		get_tree().quit()
+	else:
+		get_tree().change_scene(scene_path)
